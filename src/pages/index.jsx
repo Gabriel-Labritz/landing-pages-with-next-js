b882415f23baf9051db3a4dd819802936fc7b05e
Gabriel Labritz <gabriellabritz@gmail.com>
2023-09-14 16:29:25 -0300

    Removing comments and cleaning up some files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,21 +7,13 @@ export default function Index({ data = null }) {
   return <Home data={data} />;
 }
 
-// SSG -> Static Site Generator
-// Quase como um cache, salva os dados e entrega sempre esses arquivos prontos para o cliente = + performace
 export const getStaticProps = async () => {
-  // const raw = await fetch(
-  //   'http://localhost:1337/api/pages/?filters[slug]=olha-so-a-minha-page&populate=deep',
-  // );
-  // const json = await raw.json();
-  // const { attributes } = json.data[0];
-  // const data = mapData([attributes]);
   let data = null;
 
   try {
     data = await loadPages('olha-so-a-minha-page');
   } catch (e) {
-    //
+    data = null;
   }
 
   if (!data || !data.length) {
